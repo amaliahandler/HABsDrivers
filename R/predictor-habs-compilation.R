fc6dc75dc2cc9e2712ea630984e500050c1f76dc
Reynolds <Reynolds.Melanie@epa.gov>
2024-07-16 12:37:31 -0700

    more time, not much progress

--- R/predictor-habs-compilation.R
+++ R/predictor-habs-compilation.R
@@ -11,6 +11,10 @@ library(ggpubr)
 library(tidyr)
 library(stars)
 
+install.packages("magrittr")
+library(magrittr)
+
+
 # create mean of 2002/2007/2012 nutrient inventories, 2002 nutrient inventories from Meredith
 # reach out to meredith to see if she has the 2002 data for the same locations, create mean of all variables if possible
 
@@ -63,7 +67,33 @@ PredData12_10Ws <- read.csv("C:/Users/mreyno04/OneDrive - Environmental Protecti
 
 # combine the data by 2007 and 2012 first
 
-colnames(PredData07_10Ws) #check column names to see which ones we want to take with
+PredData2007 <- PredData07_05Ws %>%
+  bind_rows(PredData07_07Ws) %>%
+  bind_rows(PredData07_10Ws)
+
+PredData2007$Obs <- 1:nrow(PredData2007) # new column with indiv. values for each row
+
+which(PredData2007$wbCOMID == "487") # checking to make sure the data combined as intended
+
+PredData2007 %>%
+  pivot_wider(id_cols = Obs)
+
+PredData2007 %>%
+  group_by(across(where(is.factor))) %>%
+  summarise(across(where(is.numeric), mean))
+
+# by_COMID <- PredData2007 %>% group_by(wbCOMID)
+#
+# by_COMID %>% summarise(
+#   disp = mean(disp),
+#   hp = mean(hp)
+# )
+
+# here's what I want
+# if wbCOMID values are equal,
+# those rows will select
+# their values with be averaged
+# forming one new row
 
 # averaging the 2007 three data sets
 PredData2007 <- bind_rows(PredData07_05Ws, PredData07_07Ws, PredData07_10Ws) %>%
@@ -92,23 +122,25 @@ PredData2007 <- bind_rows(PredData07_05Ws, PredData07_07Ws, PredData07_10Ws) %>%
 
 head(PredData2007)
 
+
+
 # averaging the 2012 three data sets
 PredData2012 <- bind_rows(PredData12_05Ws, PredData12_07Ws, PredData12_10Ws) %>% # maybe make a loop that does this instead melanie
-  group_by(wbCOMID) %>%
+  group_by(wbCOMID) %>% # 2012 variables still named with 2007, renamed while creating new variables with the means
   summarise(X= mean(X, na.rm= TRUE),
             NPP_YrMean = mean(NPP_YrMean, na.rm= TRUE),
             NPP = mean(NPP, na.rm=TRUE),
             LST_YrMean = mean(LST_YrMean, na.rm=TRUE),
-            Tot_Ndep_2012 = mean(Tot_Ndep_2012, na.rm=TRUE),
+            Tot_Ndep_2012 = mean(Tot_Ndep_2007, na.rm=TRUE),
             Total.Input = mean(Total.Input, na.rm=TRUE),
-            N_Fert_Farm_2012 = mean(N_Fert_Farm_2012, na.rm=TRUE),
+            N_Fert_Farm_2012 = mean(N_Fert_Farm_2007, na.rm=TRUE),
             RunoffWs = mean(RunoffWs, na.rm=TRUE),
-            Atmo_Pdep_2012 = mean(Atmo_Pdep_2012, na.rm=TRUE),
-            P_Accumulated_ag_inputs_2012 = mean(P_Accumulated_ag_inputs_2012, na.rm=TRUE),
+            Atmo_Pdep_2012 = mean(Atmo_Pdep_2007, na.rm=TRUE),
+            P_Accumulated_ag_inputs_2012 = mean(P_Accumulated_ag_inputs_2007, na.rm=TRUE),
             nani = mean(nani, na.rm=TRUE),
             Precip_YrMean = mean(Precip_YrMean, na.rm=TRUE),
-            Tot_Sdep_2012 = mean(Tot_Sdep_2012, na.rm=TRUE),
-            N_CBNF_2012 = mean(N_CBNF_2012, na.rm=TRUE),
+            Tot_Sdep_2012 = mean(Tot_Sdep_2007, na.rm=TRUE),
+            N_CBNF_2012 = mean(N_CBNF_2007, na.rm=TRUE),
             WsAreaSqKm = mean(WsAreaSqKm, na.rm=TRUE),
             BFIWs = mean(BFIWs, na.rm=TRUE),
             NHDLakeDepth = mean(NHDLakeDepth, na.rm=TRUE)
@@ -118,10 +150,36 @@ head(PredData2012)
 
 # combine all predictor data to make a singular averaged dataset
 
+## Required packages
+install.packages("data.table")
+install.packages("reshape2")
+library(data.table)
+library(reshape2)
 
-#needs to be edited
-PredDataMas <- bind_rows(PredData2007, PredData2012) %>%
-  group_by(wbCOMID) %>%
+dcast.data.table(
+  merge(
+    ## melt the first data.frame and set the key as ID and variable
+    setkey(melt(as.data.table(PredData2007), id.vars = "wdCOMID"), ID, variable),
+    ## melt the second data.frame
+    melt(as.data.table(PredData2012), id.vars = "wdCOMID"),
+    ## you'll have 2 value columns...
+    all = TRUE)[, value := ifelse(
+      ## ... combine them into 1 with ifelse
+      is.na(value.x), value.y, value.x)],
+  ## This is your reshaping formula
+  ID ~ variable, value.var = "value")
+
+# PredData2007 %>%
+#   full_join(PredData2012, by = intersect(colnames(PredData2007), colnames(PredData2012))) %>%
+#   group_by(wbCOMID) %>%
+#   summarize_all(na.omit)
+
+
+# #needs to be edited
+# PredDataMas <- bind_rows(PredData2007, PredData2012)
+#
+# (df.append(df1).groupby('Movie',as_index=False, sort=False).
+#   agg({'rating':'mean', 'director':'first'}))
 
 
 
