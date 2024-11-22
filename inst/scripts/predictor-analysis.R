adacc5803ebd42ca0be01eea4e620161f7e53929
Reynolds <Reynolds.Melanie@epa.gov>
2024-11-22 12:01:46 -0800

    nutrient and depth

--- inst/scripts/predictor-analysis.R
+++ inst/scripts/predictor-analysis.R
@@ -10,18 +10,37 @@ comp_micx <- micx_pred_df
 comp_micx$nutr_all <- comp_micx$p_farm_inputs + comp_micx$n_dev_inputs
 
 comp_micx <- comp_micx %>%
-  mutate(class = factor(case_when(nutr_all > 10 & pred_micx < 0.277 ~ 'HNLM',
-                                  nutr_all < 10 & pred_micx > 0.277 ~ 'LNHM'))) %>%
-  filter(!is.na(class))
+  mutate(nutr_class = factor(case_when(nutr_all > 10 & pred_micx < 0.277 ~ 'HNLM',
+                                       nutr_all < 10 & pred_micx > 0.277 ~ 'LNHM')))
 
 # HNLM ~ high nutrients, low microcystin
-# LNHM ~ low nutrients, high microcystin
 
 HNLM <- comp_micx |>
-  filter(class == 'HNLM')
+  filter(nutr_class == 'HNLM')
+
+HNLM <- HNLM %>%
+  mutate(depth_class = factor(case_when(MAXDEPTH < 1 ~ 'D1',
+                                        MAXDEPTH > 1 & MAXDEPTH < 10 ~ 'D2',
+                                        MAXDEPTH > 10 & MAXDEPTH < 50 ~ 'D3',
+                                        MAXDEPTH > 50 ~ 'D4')))
+
+
+
+# LNHM ~ low nutrients, high microcystin
 
 LNHM <- comp_micx |>
-  filter(class == 'LNHM')
+  filter(nutr_class == 'LNHM')
+
+LNHM <- LNHM %>%
+  mutate(depth_class = factor(case_when(MAXDEPTH < 1 ~ 'D1',
+                                        MAXDEPTH > 1 & MAXDEPTH < 10 ~ 'D2',
+                                        MAXDEPTH > 10 & MAXDEPTH < 50 ~ 'D3',
+                                        MAXDEPTH > 50 ~ 'D4')))
+
+
+# Lake Depth -------------------------------------------------------------------
+
+
 
 
 
